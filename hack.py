1d603117d65ed3c11ccf85e710e46cd61a56194b
Likhach Alexander <smouzi45@gmail.com>
2020-06-21 02:07:54 +0300

    Added password generator

--- hack.py
+++ hack.py
@@ -1,16 +1,55 @@
 import socket
 import sys
+import itertools
+from string import ascii_lowercase, digits
 
-if len(sys.argv) == 4:
+max_length = 5
+max_tries = 10000000
+
+alphabet = ascii_lowercase + digits
+
+
+def get_passwords():
+    for i in range(1, max_length + 1):
+        for password in itertools.permutations(alphabet, i):
+            yield combine_tuple(password)
+        for letter in alphabet:
+            yield letter * i
+
+
+def combine_tuple(letters: tuple):
+    total = ""
+    for letter in letters:
+        total += letter
+    return total
+
+
+if len(sys.argv) == 3:
     IP = sys.argv[1]
     port = int(sys.argv[2])
-    message = sys.argv[3]
 
     with socket.socket() as my_socket:
         address = (IP, port)
         my_socket.connect(address)
+        # create generator
+        passwords = get_passwords()
+        for _i in range(max_tries):
+            try:
+                password = next(passwords)
+                data = password.encode()
+                my_socket.send(data)
+                response = my_socket.recv(1024).decode()
+                if response == "Connection success!":
+                    print(password)
+                    break
+            except StopIteration:
+                break
 
-        data = message.encode()
-        my_socket.send(data)
-        response = my_socket.recv(1024)
-        print(response.decode())
+else:
+    passwords = get_passwords()
+    for _i in range(max_tries):
+        try:
+            password = next(passwords)
+            print(password)
+        except StopIteration:
+            break
